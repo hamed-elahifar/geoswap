36aaf3650a49fe52a83cf0e63d50bc731821837a
Hamed Elahifar <hamed@xcoins.com>
2022-03-23 17:46:47 +0430

    fix farm deployment problem

--- Farm/scripts/deploy-farm.js
+++ Farm/scripts/deploy-farm.js
@@ -7,24 +7,6 @@ async function main() {
   const balance = await deployer.getBalance();
   console.log("Deployer balance:", balance.toString());
 
-  // =============================== Farm ================================================
-
-  const Farm = await ethers.getContractFactory("GeosFarm");
-  const farm = await Farm.deploy(
-    "0x93949E14587df685a8552C7c1a2F2c82EE4B9312",
-    1,
-    0,
-    10_000_000
-  );
-  console.log("Farm address:", farm.address);
-
-  const farmData = {
-    address: farm.address,
-    abi: JSON.parse(farm.interface.format("json")),
-  };
-
-  fs.writeFileSync("abi/farm.json", JSON.stringify(farmData, null, 2));
-
   // =============================== Token ================================================
   const Token = await ethers.getContractFactory("GeosSwapToken");
   const token = await Token.deploy();
@@ -36,6 +18,19 @@ async function main() {
   };
 
   fs.writeFileSync("abi/Token.json", JSON.stringify(tokenData, null, 2));
+
+  // =============================== Farm ================================================
+
+  const Farm = await ethers.getContractFactory("GeosFarm");
+  const farm = await Farm.deploy(token.address, 1, 0, 10_000_000);
+  console.log("Farm address:", farm.address);
+
+  const farmData = {
+    address: farm.address,
+    abi: JSON.parse(farm.interface.format("json")),
+  };
+
+  fs.writeFileSync("abi/farm.json", JSON.stringify(farmData, null, 2));
 }
 
 main()
